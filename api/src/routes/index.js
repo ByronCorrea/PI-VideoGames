dd377d310522ac7cac1118b1de00cd726af184d8
ByronCorrea <byron10-@hotmail.com>
2022-04-08 15:16:20 -0300

    Import && set up routers

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
+
+const videogames = require("./videogames");
+const videogame = require("./videogame");
+const genres = require("./genres");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/videogames", videogames);
+router.use("/videogame", videogame);
+router.use("/genres", genres);
+//En la consola esta roto porque configuramos las rutas sin definir las funciones de cada una (videogame, videogames, genre).js
 
 module.exports = router;
