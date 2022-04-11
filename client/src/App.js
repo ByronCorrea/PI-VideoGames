02bf3f647f8f9703329d9bfc5df9699809872f6a
ByronCorrea <byron10-@hotmail.com>
2022-04-11 17:24:20 -0300

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,17 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
-import Create from "./components/Create/Create";
-import { Route, BrowserRouter } from "react-router-dom";
-// import GameDetails from "./components/GameDetails/GameDetails";
-// import Page404 from "./components/Page404/Page404";
-// import Videogames from "./components/Videogames/Videogames";
-// import { useEffect } from "react";
+import Create from "./components/Create/Create.jsx";
+import SidePanel from "./components/SidePanel/SidePanel";
+import Home from "./components/Home/Home";
+import Detail from "./components/Detail/Detail";
+
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/*  */}
       <BrowserRouter>
-        <Route exact path="/" component={Login} />
-        <Route exat path={"/about"} component={About} />
         <Route
           path={[
             "/home",
@@ -26,10 +23,15 @@ function App() {
           ]}
           component={Nav}
         />
-        <Route exact path="/create" component={Create} />
-        {/* <Route path="*" component={Page404} /> */}
-        {/* <Route exact path="/home" component={Videogames} /> */}
-        {/* <Route exact path="/videogame/:idVideogame" component={GameDetails} />  */}
+        <Route path="/home" component={SidePanel} />
+        <Route exact path="/" component={Login} />
+        <Switch>
+          <Route exact path="/detail/:id" component={Detail} />
+          <Route exact path="/home" component={Home} />
+          <Route exat path={"/about"} component={About} />
+          <Route exact path="/create" component={Create} />
+          <Redirect to="/" />
+        </Switch>
       </BrowserRouter>
     </div>
   );
